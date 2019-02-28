5ec3f10799a13e2b4d0619811fcc9e4d91f62b38
logunlaja26 <logunlaja26@gmail.com>
2019-02-28 12:41:03 -0600

    optimized weightdata function

--- weightApp/views.py
+++ weightApp/views.py
@@ -19,8 +19,9 @@ def weightdata(request):
         else:
             print('ERROR FORM INVALID')
 
+    context = {
+        'form':form,
+        'weight_records': userWeight.objects.all()
+    }
 
-    if request.method == "GET":
-        weight_list = userWeight.objects.all()
-        weight_dict = {'weight_records':weight_list, 'form': form}
-        return render(request,'weightApp/weightdata.html',context=weight_dict)
+    return render(request,'weightApp/weightdata.html',context)
