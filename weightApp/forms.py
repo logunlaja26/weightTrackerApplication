839570ef4ddcaeffe475dc512ce7038b9b5866c0
logunlaja26 <logunlaja26@gmail.com>
2019-03-21 13:39:10 -0500

    Edited date input format

--- weightApp/forms.py
+++ weightApp/forms.py
@@ -1,7 +1,14 @@
+import datetime
 from django import forms
 from weightApp.models import userWeight
 
+
+def last_years():
+    first_year = datetime.datetime.now().year - 6
+    return list(range(first_year + 7, first_year, -1))
+
 class weightForm(forms.ModelForm):
+    entry_date = forms.DateField(widget=forms.SelectDateWidget(years = last_years()))
     class Meta():
         model = userWeight
         fields = '__all__'
